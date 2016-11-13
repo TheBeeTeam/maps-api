72168c5ebd5124daf82c8d3eb4702928897289d6
Adrian Hernandez-Mendez <adrian.hernandez@tum.de>
2016-11-13 12:25:05 +0100

    Adding cors

--- src/index.js
+++ src/index.js
@@ -11,6 +11,22 @@ let api = express();
 api.use(bodyParser.json());
 api.use(bodyParser.urlencoded({extended: true}));
 
+api.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', '*');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+});
+
+
+
 api.get("/", (req, res) => res.json({name: 'Map API'}));
 
 //App routes
